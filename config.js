720c7bc2a564f78a444786af33617825130ddb8d
birajkhatri078-cell <birajkhatri078@gmail.com>
2026-01-13 21:22:55 +0545

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dq71i1v8u/video/upload/v1768317106/samples/dance-2.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dq71i1v8u/video/upload/v1768318593/Piero_Piccioni_-_Sogni_ancora_-_La_Parmigiana_Original_Score_wnvsdr.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
